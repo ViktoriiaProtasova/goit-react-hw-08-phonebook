8465377546a67f9232ace3b9b633217910d729e1
Viktoriia Protasova <viktorka.protasova@gmail.com>
2023-08-25 23:29:55 +0300

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
 import ResponsiveAppBar from 'components/AppBar/ResponsiveAppBar';
+import CircularIndeterminate from 'Loader';
 // import { ColorRing } from 'react-loader-spinner';
 // import { LoaderWrapper } from '../components/commonCss.styled';
 
@@ -9,21 +10,7 @@ const Layout = () => {
     <>
       <ResponsiveAppBar />
 
-      <Suspense
-      // fallback={
-      //   <LoaderWrapper>
-      //     <ColorRing
-      //       visible={true}
-      //       height="80"
-      //       width="80"
-      //       ariaLabel="blocks-loading"
-      //       wrapperStyle={{}}
-      //       wrapperClass="blocks-wrapper"
-      //       colors={['#e15b64', '#f47e60', '#f8b26a', '#abbd81', '#849b87']}
-      //     />
-      //   </LoaderWrapper>
-      // }
-      >
+      <Suspense fallback={<CircularIndeterminate />}>
         <Outlet />
       </Suspense>
     </>

58cd8fe77280805906a979f814c4a76ce9dadd67
Viktoriia Protasova <viktorka.protasova@gmail.com>
2023-08-27 14:14:27 +0300

    Create auth-operations.js

---
+++ src/redux/auth/auth-operations.js
@@ -0,0 +1,126 @@
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
+
+const token = {
+  set(token) {
+    axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  },
+  unset() {
+    axios.defaults.headers.common.Authorization = '';
+  },
+};
+
+const register = createAsyncThunk(
+  'auth/register',
+  async (credentials, thunkAPI) => {
+    try {
+      const { data } = await axios.post('/users/signup', credentials);
+      token.set(data.token);
+      toast.success(`😃 Wellcome! ${data.user.name}`, {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'colored',
+      });
+      return data;
+    } catch (error) {
+      toast.error('Oops!😕Please, enter the correct data.', {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'colored',
+      });
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+const logIn = createAsyncThunk('auth/login', async (credentials, thunkAPI) => {
+  try {
+    const { data } = await axios.post('/users/login', credentials);
+    token.set(data.token);
+    toast.success(`😃 Welcome Back! ${data.user.name}`, {
+      position: 'top-right',
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'colored',
+    });
+    return data;
+  } catch (error) {
+    toast.error('Oops!😕Are You sure about this data?', {
+      position: 'top-right',
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'colored',
+    });
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
+  try {
+    await axios.post('/users/logout');
+    token.unset();
+  } catch (error) {
+    toast.error('Oops!😕Something went wrong. Try again.', {
+      position: 'top-right',
+      autoClose: 3000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'colored',
+    });
+    return thunkAPI.rejectWithValue(error.message);
+  }
+});
+
+const fetchCurrentUser = createAsyncThunk(
+  'auth/refresh',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    if (persistedToken === null) {
+      return thunkAPI.rejectWithValue();
+    }
+
+    token.set(persistedToken);
+    try {
+      const { data } = await axios.get('/users/current');
+      return data;
+    } catch (error) {
+      toast.error('Oops!😕Something went wrong. Try again.');
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+const operations = {
+  register,
+  logOut,
+  logIn,
+  fetchCurrentUser,
+};
+export default operations;

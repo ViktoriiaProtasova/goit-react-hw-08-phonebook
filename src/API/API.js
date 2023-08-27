ec4cb374a41ff19da19f070ccc292b34c8ad914f
Viktoriia Protasova <viktorka.protasova@gmail.com>
2023-08-27 14:13:22 +0300

    Update API.js

--- src/API/API.js
+++ src/API/API.js
@@ -1,33 +1,18 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
 
-// export const signupUser = async credentials => {
-//   const { data } = await axios.post(`/users/signup`, credentials);
-//   return data;
-// };
+export const getAllUserContacts = async () => {
+  const { data } = await axios('/contacts');
+  return data;
+};
 
-// export const loginUser = async credentials => {
-//   const { data } = await axios.post(`/users/login`, credentials);
-//   return data;
-// };
+export const setUserContacts = async credentials => {
+  const { data } = await axios.post('/contacts', credentials);
+  return data;
+};
 
-// export const getCurrentUser = async () => {
-//   const { data } = await axios('/users/current');
-//   return data;
-// };
-
-// export const getAllUserContacts = async () => {
-//   const { data } = await axios('/contacts');
-//   return data;
-// };
-
-// export const setUserContacts = async () => {
-//   const { data } = await axios.post('/contacts');
-//   return data;
-// };
-
-// export const removeContact = async id => {
-//   const { data } = await axios.delete(`/contacts/${id}`);
-//   return data;
-// };
+export const removeContact = async id => {
+  const { data } = await axios.delete(`/contacts/${id}`);
+  return data;
+};

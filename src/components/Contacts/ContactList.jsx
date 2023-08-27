f5aebc78257753836a0e5796145657e44f219c74
Viktoriia Protasova <viktorka.protasova@gmail.com>
2023-08-27 14:13:30 +0300

    Update ContactList.jsx

--- src/components/Contacts/ContactList.jsx
+++ src/components/Contacts/ContactList.jsx
@@ -1,12 +1,26 @@
-import { useSelector, useDispatch } from 'react-redux';
 import React from 'react';
-import { deleteContact } from 'redux/operations';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import Container from '@mui/material/Container';
+import DeleteIcon from '@mui/icons-material/Delete';
+import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
+import ListItem from '@mui/material/ListItem';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import { deleteContact, fetchContacts } from '../../redux/operations';
+import LinearIndeterminate from 'Loader';
 
 const ContactList = () => {
   const dispatch = useDispatch();
   const contacts = useSelector(state => state.contacts.items);
   const isLoading = useSelector(state => state.contacts.isLoading);
   const filter = useSelector(state => state.filter);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   const onDelete = id => dispatch(deleteContact(id));
 
@@ -19,27 +33,41 @@ const ContactList = () => {
   };
 
   return (
-    <>
-      {isLoading && (
-        <div className="spinner-border text-primary" role="status">
-          <span className="visually-hidden">Loading...</span>
-        </div>
-      )}
+    <Container
+      maxWidth="sm"
+      sx={{
+        marginTop: 2,
+        marginBottom: 5,
+        textAlign: 'center',
+      }}
+    >
+      {isLoading && <LinearIndeterminate />}
       <ul>
         {getFilteredContact()?.map(({ name, number, id }) => (
-          <li className="contactListStile" key={id} name={name} number={number}>
-            {name}: {number}
-            <button
-              className="btn btn-outline-primary deleteContactBtn"
-              type="button"
-              onClick={() => onDelete(id)}
-            >
-              Delete
-            </button>
-          </li>
+          <ListItem
+            key={id}
+            name={name}
+            number={number}
+            secondaryAction={
+              <IconButton
+                edge="end"
+                aria-label="delete"
+                onClick={() => onDelete(id)}
+              >
+                <DeleteIcon />
+              </IconButton>
+            }
+          >
+            <ListItemAvatar>
+              <Avatar>
+                <ContactPhoneIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={`${name}: ${number}`} />
+          </ListItem>
         ))}
       </ul>
-    </>
+    </Container>
   );
 };
 

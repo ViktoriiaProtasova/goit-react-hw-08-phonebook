be774a6de459bbe10f84b133a1b1b62d1df00b6a
Viktoriia Protasova <viktorka.protasova@gmail.com>
2023-08-27 14:14:14 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,26 +3,24 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import App from 'components/App';
-import { store } from './redux/store';
-// import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
 import CssBaseline from '@mui/material/CssBaseline';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
-import './index.css';
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <React.Fragment>
       <CssBaseline />
       <Provider store={store}>
-        {/* <PersistGate loading={null} persistor={persistor}> */}
-        <BrowserRouter basename="/goit-react-hw-08-phonebook">
-          <App />
-        </BrowserRouter>
-        {/* </PersistGate> */}
+        <PersistGate loading={null} persistor={persistor}>
+          <BrowserRouter basename="/goit-react-hw-08-phonebook">
+            <App />
+          </BrowserRouter>
+        </PersistGate>
       </Provider>
     </React.Fragment>
   </React.StrictMode>

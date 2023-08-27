cc7f6d5624c0c6b0ca18707bc10693c205beb63d
Viktoriia Protasova <viktorka.protasova@gmail.com>
2023-08-27 14:14:16 +0300

    Update Layout.jsx

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,16 +1,13 @@
 import { Outlet } from 'react-router-dom';
 import { Suspense } from 'react';
-import ResponsiveAppBar from 'components/AppBar/ResponsiveAppBar';
-import CircularIndeterminate from 'Loader';
-// import { ColorRing } from 'react-loader-spinner';
-// import { LoaderWrapper } from '../components/commonCss.styled';
+import ResponsiveAppBar from './components/Navigation/Navigation';
+import LinearIndeterminate from 'Loader';
 
 const Layout = () => {
   return (
     <>
       <ResponsiveAppBar />
-
-      <Suspense fallback={<CircularIndeterminate />}>
+      <Suspense fallback={<LinearIndeterminate />}>
         <Outlet />
       </Suspense>
     </>

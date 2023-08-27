2d13609168c914244b26788a33ea6876f71c9bcb
Viktoriia Protasova <viktorka.protasova@gmail.com>
2023-08-27 14:14:21 +0300

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,19 @@
+import Container from '@mui/material/Container';
 import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
 
 const HomePage = () => {
   return (
-    <div className="ContactPhoneIconWrapper">
+    <Container
+      maxWidth="sm"
+      sx={{
+        marginTop: 15,
+        marginBottom: 15,
+        textAlign: 'center',
+      }}
+    >
+      <h1>Welcome to the Phonebook!</h1>
       <ContactPhoneIcon color="primary" sx={{ fontSize: 100 }} />
-    </div>
+    </Container>
   );
 };
 
